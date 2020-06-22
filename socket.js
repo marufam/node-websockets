053d1c80e4a3759ad8ef69cc2962b4476dccf679
marufam <artdeffend@gmail.com>
2020-06-22 18:59:42 +0700

    debug

--- socket.js
+++ socket.js
@@ -1,5 +1,6 @@
 let users = [];
 const addUserWatching=(id, video)=>{
+  console.log("addUserWatching", users, id, video)
   return users.push({
     id,
     video,
@@ -7,10 +8,14 @@ const addUserWatching=(id, video)=>{
 }
 
 const getWatchingViewer=(video)=>{
+  console.log("getWatchingViewer", users, video)
+
   return users.filter((user) => user.video === video).length;
 }
 
 const removeUserWatching=(id)=>{
+  console.log("removeUserWatching", id)
+
   const index = users.findIndex((user) => user.id === id);
   if (index !== -1) {
     users.splice(index, 1)[0];
