bfba964a33fc72991c4b8d0e7b327ddee0abdf97
marufam <artdeffend@gmail.com>
2020-06-22 19:09:19 +0700

    fix socket

--- socket.js
+++ socket.js
@@ -1,4 +1,4 @@
-let users = [];
+var users = [];
 const addUserWatching=(id, video)=>{
   console.log("addUserWatching", users, id, video)
   return users.push({
@@ -9,8 +9,10 @@ const addUserWatching=(id, video)=>{
 
 const getWatchingViewer=(video)=>{
   console.log("getWatchingViewer", users, video)
-
-  return users.filter((user) => user.video === video).length;
+  uniqueArray = a => [...new Set(a.map(o => JSON.stringify(o)))].map(s => JSON.parse(s));
+  var unique = uniqueArray(users);
+  users=unique;
+  return unique.filter((user) => user.video === video).length;
 }
 
 const removeUserWatching=(id)=>{
